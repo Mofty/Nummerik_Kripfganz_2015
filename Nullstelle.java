bcfcc91d189c993f98943aeb5e3c31f557572319
Mofty <Mofty>
2015-09-26 13:18:46 +0200

    vergessen

---
+++ Nullstelle.java
@@ -0,0 +1,18 @@
+
+public class Nullstelle {
+	public static Complex getRoot(Complex start, double precision, int iterations, Polynom poly) throws InternalError {
+		Polynom ableitung = Ableitung.getDerivative(poly);
+
+		for (int i = 0; i < iterations; i++) {
+			Complex dx = Complex.div(poly.getValue(start), ableitung.getValue(start));
+			start = Complex.subtr(start, dx);
+			System.out.println(i + " : " + start);
+			if (Complex.abs(dx) < precision) {
+				return start;
+			}
+		}
+		System.out.println("max itteration erreicht"); //error
+		throw new InternalError();
+
+	}
+}
